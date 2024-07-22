c7e00e26f3eb68adea442e843f8de7e787bdef8b
Jens Ogorek <Jens.ogorek@gmail.com>
2024-07-22 15:07:22 +0200

    first serial version

---
+++ main/modules/half_duplex_serial.cpp
@@ -0,0 +1,34 @@
+#include "half_duplex_serial.h"
+#include <cstring>
+
+#define RX_BUF_SIZE 1024
+
+HalfDuplexSerial::HalfDuplexSerial(const std::string name,
+                                   const gpio_num_t rx_pin, const gpio_num_t tx_pin, const long baud_rate, const uart_port_t uart_num)
+    : Serial(name, rx_pin, tx_pin, baud_rate, uart_num) {}
+
+void HalfDuplexSerial::send_data_to_slave(uint8_t slave_id, const std::string &data) const {
+    std::string message = std::to_string(slave_id) + ":" + data;
+    uart_write_bytes(this->uart_num, message.c_str(), message.length());
+}
+
+std::string HalfDuplexSerial::read_from_uart(uint32_t timeout) const {
+    uint8_t data[RX_BUF_SIZE];
+    int len = uart_read_bytes(this->uart_num, data, RX_BUF_SIZE, timeout);
+    if (len > 0) {
+        data[len] = '\0';
+        return std::string((char *)data);
+    }
+    return "";
+}
+
+void HalfDuplexSerial::check_for_commands(uint8_t slave_id, const std::function<void(const std::string &)> &handler) const {
+    std::string data = read_from_uart();
+    if (!data.empty()) {
+        uint8_t received_id = data[0] - '0';
+        if (received_id == slave_id) {
+            std::string message = data.substr(2); // Skip the "ID:"
+            handler(message);
+        }
+    }
+}
---
+++ main/modules/half_duplex_serial.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "serial.h"
+#include <functional>
+
+class HalfDuplexSerial : public Serial {
+public:
+    HalfDuplexSerial(const std::string name,
+                     const gpio_num_t rx_pin, const gpio_num_t tx_pin, const long baud_rate, const uart_port_t uart_num);
+
+    void send_data_to_slave(uint8_t slave_id, const std::string &data) const;
+    std::string read_from_uart(uint32_t timeout = 20 / portTICK_RATE_MS) const;
+    void check_for_commands(uint8_t slave_id, const std::function<void(const std::string &)> &handler) const;
+};
